0b408e9323a9339f04a630e52611a3b0fea794fc
Budy <boed_74@yahoo.com>
2019-07-02 15:47:26 +0700

    test update

---
+++ src/main/java/ca/pledgetovote/controllers/MainController.java
@@ -0,0 +1,31 @@
+package ca.pledgetovote.controllers;
+
+import ca.pledgetovote.repositories.UserRepository;
+import ca.pledgetovote.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="/demo")
+public class MainController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(path = "/add")
+    public @ResponseBody String addNewUser(@RequestParam String name, @RequestParam String email) {
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<User> etAllUsers() {
+        return userRepository.findAll();
+    }
+}
